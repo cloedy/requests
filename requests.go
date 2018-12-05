98f07ac6fff9463ff2fb91edc9be5192ae768eaa
cloedy <skywangqj@gmail.com>
2018-12-05 14:11:00 +0800

    Update requests.go

--- requests.go
+++ requests.go
@@ -12,6 +12,17 @@ type RequestParams struct {
 	Files             io.Reader
 }
 
+var Header = map[string]string{
+	"Connection":                "keep-alive",
+	"Pragma":                    "no-cache",
+	"Cache-Control":             "no-cache",
+	"Upgrade-Insecure-Requests": "1",
+	"User-Agent":                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36",
+	"Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+	"Accept-Encoding":           "gzip, deflate",
+	"Accept-Language":           "zh-CN,zh;q=0.9,en;q=0.8",
+}
+
 func method(m string) string {
 	switch strings.ToLower(m) {
 	case "get":
